b8fb38d0a33e465464e80f6552304a146c780e29
Florence Tersier <florence.tersier@gmail.com>
2022-10-24 12:41:07 +0100

    passed

--- olist/data.py
+++ olist/data.py
@@ -9,12 +9,28 @@ class Olist:
         Its keys should be 'sellers', 'orders', 'order_items' etc...
         Its values should be pandas.DataFrames loaded from csv files
         """
-        # Hints 1: Build csv_path as "absolute path" in order to call this method from anywhere.
-            # Do not hardcode your path as it only works on your machine ('Users/username/code...')
-            # Use __file__ instead as an absolute path anchor independant of your usename
-            # Make extensive use of `breakpoint()` to investigate what `__file__` variable is really
-        # Hint 2: Use os.path library to construct path independent of Mac vs. Unix vs. Windows specificities
-        pass  # YOUR CODE HERE
+        # Create the absolute path
+        root_dir = os.path.dirname(os.path.dirname(__file__))
+        csv_path = os.path.join(root_dir, "data", "csv")
+
+        # Create the keys of the dictionnary
+        file_names = os.listdir(csv_path)
+        file_names.remove('.keep')
+
+        file_names_cleared = file_names.copy()
+        for i,file in enumerate(file_names):
+            file_names_cleared[i] = file_names_cleared[i].replace(".csv", "")
+            file_names_cleared[i] = file_names_cleared[i].replace("_dataset", "")
+            file_names_cleared[i] = file_names_cleared[i].replace("olist_", "")
+
+        # Create the dictionnary of data
+        data = {}
+
+        for (k, v) in zip(file_names_cleared, file_names):
+            data[k] = pd.read_csv(os.path.join(csv_path, v))
+
+        return data
+
 
     def ping(self):
         """
